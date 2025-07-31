bf4cd27d819a99de5afbe6c719ee381316611c03
tacherasasi <tacherasasi@gmail.com>
2025-07-31 13:00:08 +0300

    Ellie: more rust code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::env;
-use std::path::Path
+use std::path::Path;
 
 
 fn main() {
-    let args:Vec<String> = env.args().skip(1).collect();
-    println!("Hello, world!");
+    //Collect command line arguments, skipping the first one (the program name)
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    let target = if args.is_empty() {
+        env::var("HOME").expect("HOME environment variable not set");
+    }else{
+        args[0].clone();
+    };
 }
